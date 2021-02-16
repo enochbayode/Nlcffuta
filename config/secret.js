6e33f655ee781fda2500f22938c89457ec5eeca3
Enoch Bayode <bayodeenoch@gmail.com>
2021-02-16 15:03:11 -0800

    variable commit

--- config/secret.js
+++ config/secret.js
@@ -1,9 +1,9 @@
 module.exports = {
-    databaseURL : 'process.env.',
-    PORT:3000,
+    databaseURL : 'process.env.databaseURL',
+    PORT:'process.env.PORT',
 
-    GMAIL_USER: '',
-    GMAIL_PASS: ''
+    GMAIL_USER: 'process.env.GMAIL_USER',
+    GMAIL_PASS: 'process.env.GMAIL_PASS'
 }
 
 
